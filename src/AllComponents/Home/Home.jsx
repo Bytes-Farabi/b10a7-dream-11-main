b7c45d0dc522df7e01de8f33726177b53a9244d1
A-Farabi <farabisyl2017@gmail.com>
2024-10-31 02:22:23 +0600

    newsletter section added

--- src/AllComponents/Home/Home.jsx
+++ src/AllComponents/Home/Home.jsx
@@ -8,7 +8,7 @@ const Home = ({ handleActiveTab, isActive, handleCredit, credit, handleSelectedP
     return (
         <div>
             <Header credit={credit}></Header>
-            <Banner  handleCredit={handleCredit}></Banner>
+            <Banner handleCredit={handleCredit}></Banner>
             <div className="tab-row flex justify-between mt-10 items-center">
                 <div>
                     <h3 className="font-bold text-2xl">Available Players</h3>
@@ -21,7 +21,7 @@ const Home = ({ handleActiveTab, isActive, handleCredit, credit, handleSelectedP
                         </button>
 
 
-                        <button onClick={() => handleActiveTab('Selected')} className={`${isActive.available ? "btn " : 'btn rounded-none px-4 py-2 font-semibold bg-[#E7FE29] text-black'}`}>Selected 
+                        <button onClick={() => handleActiveTab('Selected')} className={`${isActive.available ? "btn " : 'btn rounded-none px-4 py-2 font-semibold bg-[#E7FE29] text-black'}`}>Selected
                             ({selectedPlayer.length})
                         </button>
                     </div>
@@ -36,35 +36,39 @@ const Home = ({ handleActiveTab, isActive, handleCredit, credit, handleSelectedP
 
             {/* shadow banner */}
 
-            {/* <div>
-                <div className="flex items-center justify-center" style={{
-          backgroundImage: `url('/assets/banner.png')`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          borderRadius: 16,
-          marginTop: 25
-        }}>
-                    <div className="bg-white rounded-lg shadow-lg p-6 max-w-lg mx-4 sm:mx-0">
-                        <h2 className="text-2xl font-semibold text-center mb-2">
-                            Subscribe to our Newsletter
-                        </h2>
-                        <p className="text-center text-gray-600 mb-6">
-                            Get the latest updates and news right in your inbox!
-                        </p>
+            <div className="border border-[#c9111115] mt-40 rounded-lg">
+                <div>
+                    <div className="flex items-center justify-center" style={{
+                        backgroundImage: `url('/assets/bg-shadow.png')`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        borderRadius: 16,
+                        padding: 20,
+                        margin: 30,
+                        border: '1px solid gray'
+                    }}>
+                        <div className="rounded-lg shadow-lg p-8 m-10 mx-4 sm:mx-0">
+                            <h2 className="text-2xl font-semibold text-center mb-2">
+                                Subscribe to our Newsletter
+                            </h2>
+                            <p className="text-center text-gray-600 mb-6">
+                                Get the latest updates and news right in your inbox!
+                            </p>
 
-                        <div className="flex items-center">
-                            <input
-                                type="email"
-                                placeholder="Enter your email"
-                                className="w-full p-3 border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                            />
-                            <button className="p-3 bg-gradient-to-r from-yellow-400 to-pink-500 text-white font-semibold rounded-r-lg hover:opacity-90 transition-opacity duration-300">
-                                Subscribe
-                            </button>
+                            <div className="flex items-center">
+                                <input
+                                    type="email"
+                                    placeholder="Enter your email"
+                                    className="w-full p-3 border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                                />
+                                <button className="p-3 bg-gradient-to-r from-yellow-400 to-pink-500 text-white font-semibold rounded-r-lg hover:opacity-90 transition-opacity duration-300">
+                                    Subscribe
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div> */}
+            </div>
 
         </div>
     );
